805a97a602f488fac8f279f28461ac4349c09b90
Santiago Morales <santiago.morales@workiasolutions.com>
2024-11-12 17:18:48 -0500

    update deploy for debug error on pipeline

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -3,11 +3,6 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "public_subnet_ids" {
-  description = "IDs de las subredes públicas creadas"
-  value       = aws_subnet.public.id
-}
-
 output "ecs_security_group_id" {
   description = "ID del grupo de seguridad para ECS"
   value       = aws_security_group.ecs_sg.id
