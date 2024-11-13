71e45b84649f70693683b6ef448991afea5e72e5
Santiago Morales <santiago.morales@workiasolutions.com>
2024-11-12 23:16:23 -0500

    fix excution pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 # backend.tf
 terraform {
   backend "s3" {
-    bucket         = "tf-s3-state-orbidi-smorales"
+    bucket         = "tf-s3-state-orbidi-sanmoral"
     key            = "terraform/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "app-state-lock"
+    dynamodb_table = "app-state-lock-sanmoral"
   }
 }
 
--- tf-s3-state/main-s3.tf
+++ tf-s3-state/main-s3.tf
@@ -2,11 +2,11 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tf-s3-state-orbidi-smorales"
+  bucket = "tf-s3-state-orbidi-sanmoral"
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "app-state-lock"
+  name           = "app-state-lock-sanmoral"
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "LockID"
