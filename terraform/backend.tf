426af24f3c08714296db9f1a11d9ece8a05d007d
Santiago Morales <santiago.morales@workiasolutions.com>
2024-11-12 23:49:01 -0500

    fix backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 # backend.tf
 terraform {
   backend "s3" {
-    bucket         = "tf-backend-states-smorales"
+    bucket         = "tf-s3-state-orbidi-sanmoral"
     key            = "test-ecs-developoment/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
